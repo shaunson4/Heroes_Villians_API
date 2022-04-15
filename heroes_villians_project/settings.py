76238ac441b337bbf35ac3449bd9a89cdcd0cd44
shaunson4 <sgray4106@gmail.com>
2022-04-15 15:49:55 -0500

    Revised Heroes Villians Project Settings file + Created Local Settings file to include MySql database properties + Removed Secret Key info to local file

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5^jas0agd75+vzhd2k03lf*3amj&j1^=*+g1*199a(j9z0d!tr'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -69,18 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -121,3 +106,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try: 
+    from heroes_villians_project.local_settings import *
+except ImportError:
+    pass 
