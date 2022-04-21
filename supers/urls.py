bfa99ad6bf3556d3c7324ed2b20282dff5ad587e
shaunson4 <sgray4106@gmail.com>
2022-04-20 23:27:52 -0500

    Revised Supers App View file to create functionality for POST request to create new super objects

--- supers/urls.py
+++ supers/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.supers_list),
+    path('<pk>/', views.supers_detail),
 
 ]
--- supers/views.py
+++ supers/views.py
@@ -1,16 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
-
-    serializer = SuperSerializer(supers, many=True)
-
-
-
-    return Response('ok')
+    if request.method == 'GET'
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      
 
