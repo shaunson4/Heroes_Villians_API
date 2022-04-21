39e6372300605509f2736f377f41a27cd32a6d09
shaunson4 <sgray4106@gmail.com>
2022-04-21 00:25:17 -0500

    Completed Delete Functionality for Super View File

--- supers/views.py
+++ supers/views.py
@@ -18,7 +18,7 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
       
-@api_view(['GET', 'PUT'])     
+@api_view(['GET', 'PUT', 'DELETE'])     
 def supers_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
@@ -29,6 +29,10 @@ def supers_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
     
    
 
